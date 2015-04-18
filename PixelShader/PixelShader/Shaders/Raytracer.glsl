8e2b5774729918e5d684bbebcc30ddcd57084bd4
Alejandro Echeverria <alejandro@echev.com>
2015-04-18 19:37:49 -0300

    Interactive raytracer implemented with reflection

--- PixelShader/PixelShader/Shaders/Raytracer.glsl
+++ PixelShader/PixelShader/Shaders/Raytracer.glsl
@@ -22,6 +22,7 @@ struct Material {
   vec4 diffuse;
   vec4 specular;
   float shininess;
+  float reflectivity;
 };
 
 struct Light {
@@ -44,14 +45,15 @@ struct Scene {
 };
 
 const int numberOfSpheres = 7;
-const int numLights = 1;
+const int numLights = 2;
 const int numberOfMaterials = 5;
+const int maxNumberOfReflections = 4;
 
 Sphere spheres[numberOfSpheres];
 Material materials[numberOfMaterials];
 Light lights[numLights];
 Camera camera = Camera(vec3(278, 273, 800));
-Scene scene = Scene(vec4(0.2,0.2,0.2,1), vec4(0,0,0,1));
+Scene scene = Scene(vec4(0.1,0.1,0.1,1), vec4(0,0,0,1));
 
 
 //ray-sphere intersection
@@ -77,90 +79,106 @@ float intersect(Ray ray, Sphere sphere)
 
 }
 
-//blinn-phong shading
-vec4 blinnPhongShading(vec3 p, vec3 n, Material material)
+//shadows
+bool isInShadow(vec3 p, Sphere sphere, Light light)
+{  
+  float lightDistance = distance(light.position, p);
+  vec3 shadowDir = normalize(light.position - p);
+  Ray shadowRay = Ray(p + 0.1 * shadowDir, shadowDir);    
+  float tShadow = intersect(shadowRay, sphere);
+  if(!isinf(tShadow) && tShadow < lightDistance)
+	return true;
+  
+  return false;
+}
+
+bool isInOtherSphereShadow(vec3 p, Sphere thisSphere, Light light)
 {
-  n = normalize(n);
+	for(int i=0; i<numberOfSpheres; i++)
+	{
+		if(isInShadow(p, spheres[i], light))
+			return true;
+	}
+	return false;
+}
+
+//blinn-phong shading
+vec4 blinnPhongShading(vec3 p, vec3 n, Sphere sphere)
+{
+  Material material	= materials[sphere.materialIndex];
   vec3 v = camera.position - p;
   v = normalize(v);
  
   vec4 shadedColor = scene.ambient * material.diffuse;
   for(int i=0; i<numLights; i++)
   {
+	vec4 lightColor = lights[i].color;
+	if(isInOtherSphereShadow(p, sphere, lights[i]))
+		lightColor = vec4(0,0,0,1);
     vec3 l = lights[i].position - p;
     l = normalize(l);  
     vec3 h = v + l;
     h = normalize(h);
 
-    shadedColor = shadedColor + lights[i].color * (dot(n,l) * material.diffuse + pow(dot(n,h), material.shininess) * material.specular);	  
+    shadedColor = shadedColor + lightColor * (max(0,dot(n,l)) * material.diffuse + pow(max(0,dot(n,h)), material.shininess) * material.specular);	  
   }
   return shadedColor;
 }  
 
-bool isInShadow(vec3 p, Sphere sphere)
-{
-  for(int i=0; i<numLights; i++)
-  {
-	float lightDistance = distance(lights[i].position, p);
-    vec3 shadowDir = normalize(lights[i].position - p);
-    Ray shadowRay = Ray(p + 0.1 * shadowDir, shadowDir);    
-    float tShadow = intersect(shadowRay, sphere);
-    if(!isinf(tShadow) && tShadow < lightDistance)
-      return true;
-  }
-  return false;
-}
 
-bool isInOtherSphereShadow(vec3 p, int thisSphereIndex)
-{
-	for(int i=0; i<numberOfSpheres; i++)
-	{
-		if(i != thisSphereIndex)
-		{
-			if(isInShadow(p, spheres[i]))
-				return true;
-		}
-	}
-	return false;
-}
 
 vec4 rayTrace(Ray ray)
 {
-  float tMin = infinity;
-  int sphereMin = -1;
-  for(int i=0; i<numberOfSpheres; i++)
-  {	
-	float t = intersect(ray, spheres[i]);
-	if(t < tMin)
-	{
-		tMin = t;
-		sphereMin = i;
-	}
-  }
-
-  if(!isinf(tMin))
+  vec4 accumulatedColor = vec4(0,0,0,1);
+  float frac = 1.0;
+  for(int i=0; i < maxNumberOfReflections; i++)
   {
-	vec3 p = ray.position + tMin*ray.direction;
-	vec3 n = p - spheres[sphereMin].position;
-	if(isInOtherSphereShadow(p, sphereMin))
-		return scene.ambient * materials[spheres[sphereMin].materialIndex].diffuse;
-	return blinnPhongShading(p, n, materials[spheres[sphereMin].materialIndex]);
-  }  
+	  float tMin = infinity;
+	  int sphereMin = -1;
+	  for(int i=0; i<numberOfSpheres; i++)
+	  {	
+		float t = intersect(ray, spheres[i]);
+		if(t < tMin)
+		{
+			tMin = t;
+			sphereMin = i;
+		}
+	  }
 
-
-  return scene.background;
+	  if(!isinf(tMin))
+	  {
+		vec3 p = ray.position + tMin*ray.direction;
+		vec3 n = normalize(p - spheres[sphereMin].position);
+		Material mat = materials[spheres[sphereMin].materialIndex];
+		vec4 localColor = blinnPhongShading(p, n, spheres[sphereMin]);
+		accumulatedColor += localColor * frac;
+		if(mat.reflectivity > 0)
+		{
+			ray.position = p;
+			ray.direction = normalize(reflect(ray.direction, n));
+			frac = mat.reflectivity;
+		}
+		else
+		{
+			break;
+		}
+	  }  
+	 
+	  accumulatedColor += scene.background * frac;
+  }
+  return accumulatedColor;
 }
 
 void init()
 { 
-  materials[0] = Material(vec4(0.156,0.126,0.507,1), vec4(1,1,1,1), 100);   // Blue specular
-  materials[1] = Material(vec4(0.656,0.626,0.107,1), vec4(0,0,0,1), 1);     // Yellow
-  materials[2] = Material(vec4(0.739, 0.725, 0.765, 1), vec4(0,0,0,1), 1);  // White
-  materials[3] = Material(vec4(0.639, 0.06, 0.062, 1), vec4(0,0,0,1), 1);   // Red
-  materials[4] = Material(vec4(0.156, 0.426, 0.107, 1), vec4(0,0,0,1), 1);  // Green
+  materials[0] = Material(vec4(0.156,0.126,0.507,1), vec4(1,1,1,1), 100, 0.3);   // Blue specular
+  materials[1] = Material(vec4(0.656,0.626,0.107,1), vec4(0,0,0,1), 1, 0.5);     // Yellow
+  materials[2] = Material(vec4(0.739, 0.725, 0.765, 1), vec4(0,0,0,1), 1, 0.5);  // White
+  materials[3] = Material(vec4(0.639, 0.06, 0.062, 1), vec4(0,0,0,1), 1, 0.5);   // Red
+  materials[4] = Material(vec4(0.156, 0.426, 0.107, 1), vec4(0,0,0,1), 1, 0.5);  // Green
 
   lights[0] = Light(vec3(0.5,1.0,0.5),  vec4(0.8,0.7,0.6,1));
-  //lights[1] = Light(vec3(250,250,-250),  vec4(0.8,0.7,0.6,1));
+  lights[1] = Light(vec3(275,540,-275),  vec4(0.2,0.2,0.2,1));
 
   spheres[0] = Sphere(vec3(180, 120, -370), 120, 0); //Blue sphere
   spheres[1] = Sphere(vec3(420, 100, -130), 100, 1); //Yellow sphere
@@ -178,7 +196,10 @@ void main(void)
   init();
 
   //Interactivity and real time stuff 
-  lights[0].position = vec3(resolution.x * mouse.x, resolution.y * mouse.y, 0.0);  //Y is flipped
+  lights[0].position = vec3(resolution.x * mouse.x, resolution.y * mouse.y, 0.0); 
+
+  spheres[0].position = vec3(275,120,-275) + 150 * vec3(sin(time),0,cos(time));
+  spheres[1].position = vec3(275,100,-275) + 120 * vec3(sin(time + 10),0,cos(time+ 10));
  
   //Ray definition
   vec3 pixel = vec3(resolution.x * pixelCoords.x, resolution.y * pixelCoords.y, 0);
