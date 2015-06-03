5ef30b10529045c234ddaacf41f2d117f5b6e193
pancha <pancha.gil@gmail.com>
2015-06-03 11:02:40 -0400

    adding api project and dummy test

---
+++ ThreeDU/ThreeAPI.Test/BoundingBoxTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using ThreeAPI;
+
+namespace ThreeAPI.Test
+{
+  [TestFixture ()]
+  public class BoundingBoxTest
+  {
+    [Test ()]
+    public void TestCase ()
+    {
+      int a = BoundingBox.dummy ();
+      Assert.AreEqual (a, 0);
+    }
+  }
+}
+
---
+++ ThreeDU/ThreeAPI/BoundingBox.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ThreeAPI
+{
+  public class BoundingBox
+  {
+    public BoundingBox ()
+    {
+    }
+
+    public static int dummy(){
+      return 1;
+    }
+  }
+}
+
