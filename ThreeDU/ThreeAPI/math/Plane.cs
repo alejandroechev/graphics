6dec380d438ef32bf2e33532ce24d1f63d72c5ab
pancha <pancha.gil@gmail.com>
2015-06-03 13:30:20 -0400

    adding bbox methods and tests

---
+++ ThreeDU/ThreeAPI/math/Plane.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using OpenTK;
+
+namespace ThreeAPI
+{
+  public class Plane
+  {
+    public Plane ()
+    {
+    }
+
+    public static bool Intersect(Plane plane, BoundingBox box){
+      return false;
+    }
+
+    public static bool Intersect(Plane plane, BoundingSphere sphere){
+      return false;
+    }
+
+    public static float Distance(Plane plane, Vector3 point){
+      return 1.0f;
+    } 
+  }
+}
+
